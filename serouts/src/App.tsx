fd212076c0d05580f1fbfc42f13b3bc9a42c0075
gbnijnugyf <1612517397@qq.com>
2022-12-24 16:00:39 +0800

    add antd

--- serouts/src/App.tsx
+++ serouts/src/App.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { PageContainer, ProLayout } from "@ant-design/pro-layout";
+import { Layout } from 'antd';
+import { Content, Footer, Header } from 'antd/es/layout/layout';
+import Sider from 'antd/es/layout/Sider';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
+        {/* <ProLayout> */}
+          <Layout className='layout'>
+            <Header>Header</Header>
+            <Layout>
+              <Sider>Sider</Sider>
+              <Content>Content</Content>
+            </Layout>
+            <Footer>Footer</Footer>
+          </Layout>
+        {/* </ProLayout> */}
+        fsdd
       </header>
     </div>
   );
