129b7aeedff80fa7a34ddbafffbc9464df958b5c
gbnijnugyf <1612517397@qq.com>
2023-12-05 23:10:38 +0800

    add comment on editPage

--- src/pages/admin/articleEdit.tsx
+++ src/pages/admin/articleEdit.tsx
@@ -9,6 +9,8 @@ import { BASEURL } from "../../globe/inter";
 export function ArticleEdit() {
   const [text, setText] = useState("# Hello Editor");
 
+  //TODO:在文章编辑过程中就会涉及图片上传回显，此时文章并未上传，
+  //为了对应————在添加文章之时就需要向后端发请求获得一个文章ID
   const onUploadImg = async (
     files: Array<File>,
     callback: (urls: string[]) => void
@@ -19,7 +21,8 @@ export function ArticleEdit() {
         return new Promise((rev, rej) => {
           const form = new FormData();
           form.append("file", file);
-
+          //该ID为该图片所对应文章ID
+          form.append("articleID", "eg:123")
           axios
             .post(BASEURL + "/api/img/upload", form, {
               headers: {
@@ -27,7 +30,7 @@ export function ArticleEdit() {
               },
             })
             .then((res) => {
-              console.log("{res}:",res);
+              console.log("{res}:", res);
               rev(res);
             })
             .catch((error) => rej(error));
