9e35aaec0896fd1417d3b0fe98bae047a4973f16
gbnijnugyf <1612517397@qq.com>
2024-01-22 21:52:33 +0800

    output by electron-package

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
+    {/* <BrowserRouter> */}
       <App />
-    </BrowserRouter>
+    {/* </BrowserRouter> */}
+    </HashRouter>
   </React.StrictMode>
 );
 
